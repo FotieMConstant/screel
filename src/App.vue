6a0384c1c1004bf4420e43630d1eba66722571fe
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2022-11-05 18:43:41 +0300

    added app update notification

--- src/App.vue
+++ src/App.vue
@@ -106,4 +106,18 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
+/* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
 </style>
