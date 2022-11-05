0320444ce8e6363e067cb15990da0fb1283f51fc
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2022-11-05 18:46:26 +0300

    added app update notification

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    {{ $t("gettings.morning") }}, Fotie
+    {{ $t("gettings.morning") }}, Fotie...
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
