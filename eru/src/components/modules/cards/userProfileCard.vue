fbf61b7a52adfe5a429f6b8f1dd1ac906ad50e6b
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2022-12-13 19:51:29 +0300

    done with the userProfileCard component

---
+++ eru/src/components/modules/cards/userProfileCard.vue
@@ -0,0 +1,99 @@
+<template>
+  <div
+    class="dark:bg-gray-800 dark:text-gray-50 text-sky-white bg-grayLightMode-100 rounded-curl"
+  >
+    <div
+      style="background-size: cover"
+      :style="{
+        'background-image':
+          'linear-gradient( to right, rgba(0,0,0, 0.55), #161B22 ), url(' +
+          coverImage +
+          ')',
+      }"
+      class="w-full h-28 rounded-t-curl"
+    ></div>
+    <div class="p-5 -mt-16 text-center">
+      <div class="space-y-1">
+        <div class="relative w-20 mx-auto">
+          <img class="h-20 rounded-full" :src="profileImage" alt="" />
+          <div
+            v-show="isOnline"
+            class="w-4 h-4 bg-blue-accent rounded-full my-auto border-2 absolute right-1 bottom-1 dark:border-gray-800 border-grayLightMode-100"
+          ></div>
+        </div>
+        <div class="">
+          <div class="dark:text-gray-100 text-grayLightMode-400 font-bold">
+            {{ name }}
+          </div>
+          <div class="text-gray-300 text-sm">{{ userName }}</div>
+        </div>
+        <div class="flex space-x-1 justify-center">
+          <basicChip class="mt-1 text-xs" text="SS" />
+          <basicChip class="mt-1 text-xs" text="OW" />
+        </div>
+      </div>
+      <div class="my-2.5 text-gray-300">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit..dolor sit amet,
+        co... ⚡️
+      </div>
+      <div
+        class="border-y dark:border-gray-700 border-grayLightMode-200 flex justify-between px-10 py-3"
+      >
+        <div class="">
+          <div class="dark:text-gray-100 font-bold text-grayLightMode-400">
+            1.4k
+          </div>
+          <div class="dark:text-gray-300 font-medium text-grayLightMode-300">
+            followers
+          </div>
+        </div>
+        <div
+          class="border-r dark:border-gray-700 border-grayLightMode-200"
+        ></div>
+        <div class="">
+          <div class="dark:text-gray-100 font-bold text-grayLightMode-400">
+            3.4k
+          </div>
+          <div class="dark:text-gray-300 font-medium text-grayLightMode-300">
+            following
+          </div>
+        </div>
+      </div>
+      <div class="text-center font-bold text-blue-accent mt-4 cursor-pointer">
+        My Profile
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import basicChip from "@/components/modules/chips/basicChip.vue";
+
+export default {
+  name: "userProfileCard",
+  components: { basicChip },
+  props: {
+    coverImage: {
+      type: String,
+      default: "https://images.unsplash.com/photo-1520564816385-4f9d711941aa",
+    },
+    profileImage: {
+      type: String,
+      default: "https://i.pravatar.cc/300",
+    },
+    name: {
+      type: String,
+      default: "null",
+    },
+    userName: {
+      type: String,
+      default: "@null",
+    },
+    isOnline: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+<style lang="scss" scoped></style>
--- eru/src/views/HomeView.vue
+++ eru/src/views/HomeView.vue
@@ -3,7 +3,9 @@
     <div class="min-h-screen flex flex-col h-screen">
       <!-- main container -->
       <div class="flex-1 flex flex-row space-x-4 overflow-y-hidden">
-        <div class="w-3/12 overflow-y-auto">Sidebar</div>
+        <div class="w-3/12 overflow-y-auto pt-6 mt-16">
+          <userProfileCard name="Arnau Jiménez" userName="@ajmnz" isOnline />
+        </div>
         <main class="w-6/12 h-screen pb-16 pt-6 mt-16 overflow-y-scroll">
           <!-- post screel -->
           <createScreelCard />
@@ -34,6 +36,7 @@ import createScreelCard from "@/components/modules/cards/createScreelCard.vue";
 import cardPost from "@/components/modules/cards/cardPost.vue";
 import peopleAcrossTheGlobe from "@/components/global/peopleAcrossTheGlobe.vue";
 import smallAdsCard from "@/components/modules/cards/smallAdsCard.vue";
+import userProfileCard from "@/components/modules/cards/userProfileCard.vue";
 
 export default {
   name: "HomeView",
@@ -42,6 +45,7 @@ export default {
     cardPost,
     peopleAcrossTheGlobe,
     smallAdsCard,
+    userProfileCard,
   },
 };
 </script>
