89308570a06c9e632381b5eb5e57a840dbb71c75
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2023-01-11 17:14:54 +0300

    worked on the src/components/modules/modals/OverlayNotifications.vue

--- eru/src/store/modules/authentication/state.js
+++ eru/src/store/modules/authentication/state.js
@@ -1,15 +1,3 @@
 export const state = () => ({
-  currentUser: {
-    _id: "63be73ae47f18ca522039b62",
-    provider_id: 42372656,
-    provider_name: "github",
-    name: "fotiecodes",
-    email: "fotiemoghommahieconstant@gmail.com",
-    avatar: "https://avatars.githubusercontent.com/u/42372656?v=4",
-    expiresIn: null,
-    updated_at: "2023-01-11T08:30:38.090000Z",
-    created_at: "2023-01-11T08:30:38.090000Z",
-    token:
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI2M2JlNzNkY2U3ZDk0ZGI5YzcwNjkxZDIiLCJqdGkiOiI2Y2E0NGEwY2JmNzRhNTdkNmQ2NjFmMmZiYTk5MGQyY2M4ODA1MDg1YWI1YjYzMjA5ODBlMzYwN2EyYTQzZDczOTI1ODU1MjIwYWJhZTk5OCIsImlhdCI6MTY3MzQyNTk2MC4wMTAxOTMsIm5iZiI6MTY3MzQyNTk2MC4wMTAxOTUsImV4cCI6MTcwNDk2MTk2MC4wMDkzMDUsInN1YiI6IjYzYmU3M2FlNDdmMThjYTUyMjAzOWI2MiIsInNjb3BlcyI6W119.H_ZSAOD3viDA4Toc3vYiQVChfqC1o1eq5o0C3rkCIPLiq6pGclFLLC0JWhprbb0cnIskeQ-PvalZEQMeh2QXIvPLNLv6Fg-HTTbaFmaC4nvBpS_5LzB_io7tTW6PYuEyDgCYPQlUcR2ylgMA0waFr2B5F2exwSsvcXbWMCZ4gme8KlOIe2V6LNGMLYHkSzrHwZulfXke77VZdu-0adsPG-bVpuDmF3KpplV_iBsPIBDGJLl-SAJgPWdY0zupvuV75WxByIOwzZ0B_fYpyE7guT_vUo7N2LdAg8DmUoN0r2CQ7B7vBVB1JDHPDHT271M6_0ytqwl4UtvQ5jK7aAX4ucvv9BcmG-BbdEOPOQDLQGfzb5C4j5dOpz7nxvCfIyG0OdRkg20-seLmPL_V8Up26z6FLB6zhzlqMq9LalTeV64b4mJz8dC7LXr1xGhmXXAAMInymyKzojuzdTLU-fPh3jXtpmlGCPKP389bU7uNfTA3aNo5DB6dWZo_c02WuJ_c-nOzgKCIW3b9BQwfXVDpxmXV00lvok0a7ImUZW0XtpZq1i8yOOwpQM_gwlBByQRl0bVq1bR3PCW8e_6Un_BK_pBkCxU64aZPYfjq_lMXxJXzFUh1s5chc0lsVLhmgzckVmlUc26VN_N6yiYw9heGV9_bmRK3JwXnGvu6v2fwV2A",
-  }, // current loggedin user
+  currentUser: null, // current loggedin user
 });
