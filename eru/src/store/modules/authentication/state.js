246e104e9e552461175aa4baceda0dc2954b4ef6
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2023-01-14 11:37:39 +0300

    commented out store user

--- eru/src/store/modules/authentication/state.js
+++ eru/src/store/modules/authentication/state.js
@@ -1,16 +1,16 @@
 export const state = () => ({
   access_token: null,
-  // currentUser: null, // current loggedin user
-  currentUser: {
-    _id: "63c165e2e6a0b47cc70489d2",
-    provider_id: 42372656,
-    provider_name: "github",
-    name: "fotiecodes",
-    email: "fotiemoghommahieconstant@gmail.com",
-    username: "FotieMConstant",
-    avatar: "https://avatars.githubusercontent.com/u/42372656?v=4",
-    expiresIn: null,
-    updated_at: "2023-01-13T14:08:34.234000Z",
-    created_at: "2023-01-13T14:08:34.234000Z",
-  }, // current loggedin user
+  currentUser: null, // current loggedin user
+  // currentUser: {
+  //   _id: "63c165e2e6a0b47cc70489d2",
+  //   provider_id: 42372656,
+  //   provider_name: "github",
+  //   name: "fotiecodes",
+  //   email: "fotiemoghommahieconstant@gmail.com",
+  //   username: "FotieMConstant",
+  //   avatar: "https://avatars.githubusercontent.com/u/42372656?v=4",
+  //   expiresIn: null,
+  //   updated_at: "2023-01-13T14:08:34.234000Z",
+  //   created_at: "2023-01-13T14:08:34.234000Z",
+  // }, // current loggedin user
 });
