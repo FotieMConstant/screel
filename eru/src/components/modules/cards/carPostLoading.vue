85fff430fbd27534ee64ea1c2c8f05f18e7a4b66
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2023-01-15 16:50:49 +0300

    worked ont he loader for the screels

---
+++ eru/src/components/modules/cards/carPostLoading.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="cursor-progress dark:bg-gray-800 dark:text-gray-100 text-grayLightMode-400 bg-grayLightMode-100 p-5 rounded-curl"
+  >
+    <div class="animate-pulse flex space-x-4">
+      <div
+        class="rounded-full dark:bg-gray-600 bg-grayLightMode-200 h-10 w-10"
+      ></div>
+      <div class="flex-1 space-y-6 py-1">
+        <div class="flex justify-between">
+          <div
+            class="h-2 w-44 dark:bg-gray-600 bg-grayLightMode-200 rounded"
+          ></div>
+          <div
+            class="h-2 w-20 dark:bg-gray-600 bg-grayLightMode-200 rounded"
+          ></div>
+        </div>
+        <div class="space-y-3">
+          <div class="h-2 dark:bg-gray-600 bg-grayLightMode-200 rounded"></div>
+          <div class="h-2 dark:bg-gray-600 bg-grayLightMode-200 rounded"></div>
+          <div
+            class="h-2 w-60 dark:bg-gray-600 bg-grayLightMode-200 rounded"
+          ></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "carPostLoading",
+  props: {},
+};
+</script>
--- eru/src/views/HomeView.vue
+++ eru/src/views/HomeView.vue
@@ -4,7 +4,29 @@
     <!-- post screel -->
     <createScreelCard />
     <!--/ post screel -->
+    <!-- divider -->
+    <div class="text-center">
+      <span
+        class="inline-block w-1 h-1 rounded-full dark:bg-blue-800 bg-blue-100 ml-1"
+      ></span>
+      <span
+        class="inline-block w-3 h-1 rounded-full dark:bg-blue-700 bg-blue-200 ml-1"
+      ></span>
+      <span
+        class="inline-block w-20 h-1 rounded-full dark:bg-blue-600 bg-blue-300 ml-1"
+      ></span>
+      <span
+        class="inline-block w-3 h-1 rounded-full dark:bg-blue-700 bg-blue-200 ml-1"
+      ></span>
+      <span
+        class="inline-block w-1 h-1 rounded-full dark:bg-blue-800 bg-blue-100 ml-1"
+      ></span>
+    </div>
+    <!--/ divider -->
     <!-- card post  -->
+    <div class="mt-1.5 space-y-4">
+      <carPostLoading v-for="n in 2" :key="n" />
+    </div>
     <div class="mt-4 space-y-4">
       <cardPost v-for="n in 20" :key="n" />
     </div>
@@ -18,13 +40,14 @@
 import DefaultLayout from "@/components/Layout/DefaultLayout";
 import createScreelCard from "@/components/modules/cards/createScreelCard.vue";
 import cardPost from "@/components/modules/cards/cardPost.vue";
-
+import carPostLoading from "@/components/modules/cards/carPostLoading.vue";
 export default {
   name: "HomeView",
   components: {
     DefaultLayout,
     createScreelCard,
     cardPost,
+    carPostLoading,
   },
 };
 </script>
