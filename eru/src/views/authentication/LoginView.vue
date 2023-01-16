f0d1db91120f98012b6d058b04bbcdddbc2ff986
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2023-01-16 16:58:50 +0300

    🛠added a banner to login for first time commers

--- eru/src/views/authentication/LoginView.vue
+++ eru/src/views/authentication/LoginView.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="">
+    <!-- top banner -->
+    <BannerInformation
+      localKey="infoBeta"
+      title="Screel beta:"
+      text="Expect bugs, we're nerding and geeking out to fix them. Thanks for your patience!"
+    />
+    <!-- /top banner -->
     <LoginAuth class="absolute inset-0 m-auto z-0" />
     <FooterAuth class="absolute bottom-0 m-auto z-0 p-5" />
   </div>
@@ -8,10 +15,10 @@
 <script>
 import LoginAuth from "@/components/global/auth/LoginAuth.vue";
 import FooterAuth from "@/components/global/auth/FooterAuth.vue";
-
+import BannerInformation from "@/components/global/BannerInformation.vue";
 export default {
   name: "LoginView",
-  components: { LoginAuth, FooterAuth },
+  components: { LoginAuth, FooterAuth, BannerInformation },
 };
 </script>
 
